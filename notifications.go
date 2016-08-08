1ed020ecb87a66c1a87299c3ccc8a986271c1c53
Dmitri Shuralyov <shurcooL@gmail.com>
2016-08-07 23:00:08 -0700

    Allow shurcooL to see his GitHub notifications in addition to local.
    For convenience, all my notifications appear in one place. :)

--- notifications.go
+++ notifications.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/google/go-github/github"
+	"github.com/gregjones/httpcache"
 	"github.com/shurcooL/notifications"
 	"github.com/shurcooL/notifications/fs"
+	"github.com/shurcooL/notifications/githubapi"
 	"github.com/shurcooL/notificationsapp"
 	"github.com/shurcooL/notificationsapp/common"
 	"github.com/shurcooL/users"
 	"golang.org/x/net/context"
 	"golang.org/x/net/webdav"
+	"golang.org/x/oauth2"
 )
 
 // initNotifications creates and returns a notification service,
 // and registers handlers for the notifications app.
 func initNotifications(root webdav.FileSystem, users users.Service) (notifications.ExternalService, error) {
 	service := fs.NewService(root, users)
+
+	authTransport := &oauth2.Transport{
+		Source: oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("HOME_GH_SHURCOOL_NOTIFICATIONS")}),
+	}
+	cacheTransport := &httpcache.Transport{
+		Transport:           authTransport,
+		Cache:               httpcache.NewMemoryCache(),
+		MarkCachedResponses: true,
+	}
+	var shurcoolGitHubNotifications notifications.InternalService = githubapi.NewService(
+		github.NewClient(&http.Client{Transport: authTransport}),
+		github.NewClient(&http.Client{Transport: cacheTransport}),
+	)
+
+	shurcoolSeeHisGitHubNotificationsService := shurcoolSeeHisGitHubNotifications{
+		service:                     service,
+		shurcoolGitHubNotifications: shurcoolGitHubNotifications,
+		users: users,
+	}
 
 	opt := notificationsapp.Options{
 		Context: func(req *http.Request) context.Context {
@@ -55,7 +79,7 @@ func initNotifications(root webdav.FileSystem, users users.Service) (notificatio
 	if *productionFlag {
 		opt.HeadPre += "\n\t\t" + googleAnalytics
 	}
-	notificationsApp := notificationsapp.New(service, users, opt)
+	notificationsApp := notificationsapp.New(shurcoolSeeHisGitHubNotificationsService, users, opt)
 
 	notificationsHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// TODO: Factor this out?
@@ -84,4 +108,50 @@ func initNotifications(root webdav.FileSystem, users users.Service) (notificatio
 	http.Handle("/notifications/", notificationsHandler)
 
 	return service, nil
+}
+
+// shurcoolSeeHisGitHubNotifications lets shurcooL also see his GitHub notifications,
+// in addition to local ones.
+type shurcoolSeeHisGitHubNotifications struct {
+	service                     notifications.InternalService
+	shurcoolGitHubNotifications notifications.InternalService
+	users                       users.Service
+}
+
+func (s shurcoolSeeHisGitHubNotifications) List(ctx context.Context, opt interface{}) (notifications.Notifications, error) {
+	var nss notifications.Notifications
+	ns, err := s.service.List(ctx, opt)
+	if err != nil {
+		return nss, err
+	}
+	nss = append(nss, ns...)
+
+	if currentUser, err := s.users.GetAuthenticatedSpec(ctx); err == nil && currentUser == shurcool {
+		ns, err := s.shurcoolGitHubNotifications.List(ctx, opt)
+		if err != nil {
+			return nss, err
+		}
+		nss = append(nss, ns...)
+	}
+
+	return nss, nil
+}
+
+func (s shurcoolSeeHisGitHubNotifications) Count(ctx context.Context, opt interface{}) (uint64, error) {
+	var count uint64
+	n, err := s.service.Count(ctx, opt)
+	if err != nil {
+		return count, err
+	}
+	count += n
+
+	if currentUser, err := s.users.GetAuthenticatedSpec(ctx); err == nil && currentUser == shurcool {
+		n, err := s.shurcoolGitHubNotifications.Count(ctx, opt)
+		if err != nil {
+			return count, err
+		}
+		count += n
+	}
+
+	return count, nil
 }
