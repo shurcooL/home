1a4444beebe18767a6054b3dacb02b7821688f94
Dmitri Shuralyov <shurcooL@gmail.com>
2016-12-30 15:14:35 -0500

    idiomaticgo: Add regression test for RenderBodyInnerHTML.

--- idiomaticgo/render_test.go
+++ idiomaticgo/render_test.go
@@ -1,7 +1,9 @@
 package idiomaticgo_test
 
 import (
+	"bytes"
 	"context"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,8 +14,54 @@ import (
 	"github.com/shurcooL/issues/fs"
 	"github.com/shurcooL/notifications"
 	"github.com/shurcooL/users"
+	"github.com/shurcooL/webdavfs/webdavfs"
 	"golang.org/x/net/webdav"
 )
+
+var updateFlag = flag.Bool("update", false, "Update golden files.")
+
+// issuesFS was generated with:
+//
+// 	goexec 'vfsgen.Generate(filter.Keep(http.Dir(filepath.Join(os.Getenv("HOME"), "Dropbox", "Store", "issues")), func(path string, fi os.FileInfo) bool { return (path == "/" || path == "/dmitri.shuralyov.com" || path == "/dmitri.shuralyov.com/idiomatic-go" || strings.HasPrefix(path, "/dmitri.shuralyov.com/idiomatic-go/")) && fi.Name() != ".DS_Store"}), vfsgen.Options{PackageName: "idiomaticgo_test", Filename: "issuesfs_vfsdata_test.go", VariableName: "issuesFS", VariableComment: "issuesFS is issues test data."})'
+
+// TestBodyInnerHTML verifies that idiomaticgo.RenderBodyInnerHTML renders the body inner HTML as expected.
+func TestBodyInnerHTML(t *testing.T) {
+	users := mockUsers{}
+	notifications := mockNotifications{}
+	issues, err := fs.NewService(
+		webdavfs.New(issuesFS),
+		notifications, users)
+	if err != nil {
+		t.Fatal(err)
+	}
+	authenticatedUser, err := users.GetAuthenticated(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	returnURL := "http://localhost:8080/idiomatic-go"
+
+	var buf bytes.Buffer
+	err = idiomaticgo.RenderBodyInnerHTML(context.Background(), &buf, issues, notifications, authenticatedUser, returnURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := buf.Bytes()
+	if *updateFlag {
+		err := ioutil.WriteFile(filepath.Join("testdata", "body-inner.html"), got, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return
+	}
+
+	want, err := ioutil.ReadFile(filepath.Join("testdata", "body-inner.html"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(got, want) {
+		t.Error("idiomaticgo.RenderBodyInnerHTML produced output that doesn't match 'testdata/body-inner.html'")
+	}
+}
 
 func BenchmarkRenderBodyInnerHTML(b *testing.B) {
 	users := mockUsers{}
