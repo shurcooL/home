252a53a62f7d5bae0e06cae5bfc151019d6b84c3
Dmitri Shuralyov <shurcooL@gmail.com>
2017-01-23 17:18:43 -0500

    Remove unused CSS from index page.

--- index.go
+++ index.go
@@ -30,8 +30,6 @@ var indexHTML = template.Must(template.New("").Parse(`<html>
 	<head>
 		<title>Dmitri Shuralyov</title>
 		<link href="/icon.png" rel="icon" type="image/png">
-		<link href="/blog/assets/octicons/octicons.min.css" rel="stylesheet" type="text/css">
-		<link href="/blog/assets/gfm/gfm.css" rel="stylesheet" type="text/css">
 		<link href="/assets/index/style.css" rel="stylesheet" type="text/css">
 		{{if .Production}}` + googleAnalytics + `{{end}}
 	</head>
