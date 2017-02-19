6d7a7d6e10a59fa9f3837023d57492be882a29bd
Dmitri Shuralyov <shurcooL@gmail.com>
2017-02-19 14:10:53 -0500

    Remove commented out code.
    It won't be needed anymore.

--- assets/assets.go
+++ assets/assets.go
@@ -23,8 +23,7 @@ func importPathToDir(importPath string) string {
 
 // Assets contains assets for home.
 var Assets = union.New(map[string]http.FileSystem{
-	"/assets": gopherjs_http.NewFS(http.Dir(importPathToDir("github.com/shurcooL/home/assets/_data"))),
-	//"/octicons": octicons.Assets,
+	"/assets":     gopherjs_http.NewFS(http.Dir(importPathToDir("github.com/shurcooL/home/assets/_data"))),
 	"/resume.js":  gopherjs_http.Package("github.com/shurcooL/resume/frontend"),
 	"/resume.css": vfsutil.File(filepath.Join(importPathToDir("github.com/shurcooL/resume/frontend"), "style.css")),
 })
