66c5e6798e0f79f5bf64a5d6959ae18146c4c623
Dmitri Shuralyov <shurcooL@gmail.com>
2017-05-30 19:06:02 -0400

    blog, idiomaticgo: Remove unused struct field.
    Follows shurcooL/reactions@6483de1b635901bb9e7f85db4d2ea41dad193872.

--- idiomaticgo/render.go
+++ idiomaticgo/render.go
@@ -124,11 +124,10 @@ It'll show up here when I add an "Accepted" label.`)))
 		io.WriteString(w, `</div>`)
 
 		io.WriteString(w, `<div class="reaction-bar-appear" style="display: flex; justify-content: space-between; margin-bottom: 60px;">`)
-		err = htmlg.RenderComponents(w, resumecomponent.ReactionsBar{
-			Reactions:    comment.Reactions,
-			ReactableURL: ReactableURL,
-			CurrentUser:  authenticatedUser,
-			ID:           fmt.Sprintf("%d", issue.ID), // TODO: "/0"?
+		err = htmlg.RenderComponents(w, reactionscomponent.ReactionsBar{
+			Reactions:   comment.Reactions,
+			CurrentUser: authenticatedUser,
+			ID:          fmt.Sprintf("%d", issue.ID), // TODO: "/0"?
 		})
 		if err != nil {
 			return err
