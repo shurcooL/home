db6816cfcfad0e0aac424eada9ea0971dbd20450
Dmitri Shuralyov <dmitri@shuralyov.com>
2018-05-15 18:15:15 -0400

    httputil: Update HeaderWriter documentation.
    Taken from current http.ResponseWriter documentation.

--- httputil/header.go
+++ httputil/header.go
@@ -5,11 +5,25 @@ import "net/http"
 // HeaderWriter interface is used to construct an HTTP response header and trailer.
 type HeaderWriter interface {
 	// Header returns the header map that will be sent by
-	// WriteHeader. Changing the header after a call to
-	// WriteHeader (or Write) has no effect unless the modified
-	// headers were declared as trailers by setting the
-	// "Trailer" header before the call to WriteHeader (see example).
-	// To suppress implicit response headers, set their value to nil.
+	// WriteHeader. The Header map also is the mechanism with which
+	// Handlers can set HTTP trailers.
+	//
+	// Changing the header map after a call to WriteHeader (or
+	// Write) has no effect unless the modified headers are
+	// trailers.
+	//
+	// There are two ways to set Trailers. The preferred way is to
+	// predeclare in the headers which trailers you will later
+	// send by setting the "Trailer" header to the names of the
+	// trailer keys which will come later. In this case, those
+	// keys of the Header map are treated as if they were
+	// trailers. See the example. The second way, for trailer
+	// keys not known to the Handler until after the first Write,
+	// is to prefix the Header map keys with the TrailerPrefix
+	// constant value. See TrailerPrefix.
+	//
+	// To suppress implicit response headers (such as "Date"), set
+	// their value to nil.
 	Header() http.Header
 }
 
