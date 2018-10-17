1771d6e32fa43c7c723a46284046fa3d95283ec9
Dmitri Shuralyov <dmitri@shuralyov.com>
2018-10-17 00:27:12 -0400

    internal/code: Extend test to cover pushing new commit.
    The newly pushed packages aren't being discovered yet.
    That will be added in the next commit.
    
    Updates #19.

--- internal/code/code_test.go
+++ internal/code/code_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -38,6 +40,19 @@ func Test(t *testing.T) {
 	if err != nil {
 		t.Fatal("code.NewService:", err)
 	}
+
+	gitHandler, err := code.NewGitHandler(service, filepath.Join(tempDir, "repositories"), events, users, nil, func(req *http.Request) *http.Request { return req })
+	if err != nil {
+		t.Fatal("code.NewGitHandler:", err)
+	}
+	httpServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if ok := gitHandler.ServeGitMaybe(w, req); ok {
+			return
+		}
+		t.Error("http server got a non-git request")
+		http.NotFound(w, req)
+	}))
+	defer httpServer.Close()
 
 	// Test initial state.
 	{
@@ -130,7 +145,7 @@ The PNG specification is at <a href="http://www.w3.org/TR/PNG/">http://www.w3.or
 		}
 		got := service.List()
 		if !reflect.DeepEqual(got, want) {
-			t.Error("not equal")
+			t.Error("initial state: not equal")
 		}
 	}
 
@@ -238,7 +253,117 @@ The PNG specification is at <a href="http://www.w3.org/TR/PNG/">http://www.w3.or
 		}
 		got := service.List()
 		if !reflect.DeepEqual(got, want) {
-			t.Error("not equal")
+			t.Error("after empty repository created: not equal")
+		}
+	}
+
+	// Push a copy of scratch repository to the new repository.
+	{
+		cmd := exec.Command("git", "push", httpServer.URL+"/new/repo", "master:master")
+		cmd.Dir = filepath.Join("testdata", "repositories", "dmitri.shuralyov.com", "scratch")
+		err = cmd.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Test after new repository pushed to.
+	{
+		want := []*code.Directory{
+			{
+				ImportPath:   "dmitri.shuralyov.com/emptyrepo",
+				RepoRoot:     "dmitri.shuralyov.com/emptyrepo",
+				RepoPackages: 0,
+			},
+			{
+				ImportPath:   "dmitri.shuralyov.com/kebabcase",
+				RepoRoot:     "dmitri.shuralyov.com/kebabcase",
+				RepoPackages: 1,
+				Package: &code.Package{
+					Name:     "kebabcase",
+					Synopsis: "Package kebabcase provides a parser for identifier names using kebab-case naming convention.",
+					DocHTML: `<p>
+Package kebabcase provides a parser for identifier names
+using kebab-case naming convention.
+</p>
+<p>
+Reference: <a href="https://en.wikipedia.org/wiki/Naming_convention_(programming)#Multiple-word_identifiers">https://en.wikipedia.org/wiki/Naming_convention_(programming)#Multiple-word_identifiers</a>.
+</p>
+`,
+				},
+			},
+			{
+				ImportPath:   "dmitri.shuralyov.com/new/repo",
+				RepoRoot:     "dmitri.shuralyov.com/new/repo",
+				RepoPackages: 0,
+			},
+			{
+				ImportPath:   "dmitri.shuralyov.com/scratch",
+				RepoRoot:     "dmitri.shuralyov.com/scratch",
+				RepoPackages: 4,
+				LicenseRoot:  "dmitri.shuralyov.com/scratch",
+				Package: &code.Package{
+					Name:     "scratch",
+					Synopsis: "Package scratch is used for testing.",
+					DocHTML: `<p>
+Package scratch is used for testing.
+</p>
+`,
+				},
+			},
+			{
+				ImportPath:   "dmitri.shuralyov.com/scratch/hello",
+				RepoRoot:     "dmitri.shuralyov.com/scratch",
+				RepoPackages: 4,
+				LicenseRoot:  "dmitri.shuralyov.com/scratch",
+				Package: &code.Package{
+					Name: "main",
+				},
+			},
+			{
+				ImportPath:   "dmitri.shuralyov.com/scratch/image",
+				RepoRoot:     "dmitri.shuralyov.com/scratch",
+				RepoPackages: 4,
+				LicenseRoot:  "dmitri.shuralyov.com/scratch/image",
+			},
+			{
+				ImportPath:   "dmitri.shuralyov.com/scratch/image/jpeg",
+				RepoRoot:     "dmitri.shuralyov.com/scratch",
+				RepoPackages: 4,
+				LicenseRoot:  "dmitri.shuralyov.com/scratch/image",
+				Package: &code.Package{
+					Name:     "jpeg",
+					Synopsis: "Package jpeg implements a tiny subset of a JPEG image decoder and encoder.",
+					DocHTML: `<p>
+Package jpeg implements a tiny subset of a JPEG image decoder and encoder.
+</p>
+<p>
+JPEG is defined in ITU-T T.81: <a href="http://www.w3.org/Graphics/JPEG/itu-t81.pdf">http://www.w3.org/Graphics/JPEG/itu-t81.pdf</a>.
+</p>
+`,
+				},
+			},
+			{
+				ImportPath:   "dmitri.shuralyov.com/scratch/image/png",
+				RepoRoot:     "dmitri.shuralyov.com/scratch",
+				RepoPackages: 4,
+				LicenseRoot:  "dmitri.shuralyov.com/scratch/image",
+				Package: &code.Package{
+					Name:     "png",
+					Synopsis: "Package png implements a tiny subset of a PNG image decoder and encoder.",
+					DocHTML: `<p>
+Package png implements a tiny subset of a PNG image decoder and encoder.
+</p>
+<p>
+The PNG specification is at <a href="http://www.w3.org/TR/PNG/">http://www.w3.org/TR/PNG/</a>.
+</p>
+`,
+				},
+			},
+		}
+		got := service.List()
+		if !reflect.DeepEqual(got, want) {
+			t.Error("after new repository pushed to: not equal")
 		}
 	}
 }
