abbc719fee536a7ad933889c06e2943887ebd16f
Dmitri Shuralyov <dmitri@shuralyov.com>
2019-01-01 02:11:59 -0500

    about: 2019 is the Year of the Gopher
    Following the longstanding tradition. For example,
    see golang/go@c6282e7ac5a73809b8423a2abc45940148eb5bc7.

--- about.go
+++ about.go
@@ -141,7 +141,7 @@ func initAbout(notifications notifications.Service, users users.Service) {
 					elem.P(
 						"Coming from a game development and graphics/UI background where C++ was used predominantly, ",
 						"he discovered and made a full switch to Go ",
-						elem.Abbr(attr.Title("2013."), "five years ago"),
+						elem.Abbr(attr.Title("2013."), "six years ago"),
 						", which lead to increased developer happiness.",
 					),
 					elem.P(
